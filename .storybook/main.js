62a361dfe41a1136cd4092c964669cecef6c2591
oh min seok <oh930428@gmail.com>
2022-05-12 01:16:52 +0900

    [package]@storybook/addon-docs 추가

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,13 @@
 module.exports = {
-  stories: ['../src/stories/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: [
+    '../src/stories/**/*.stories.mdx',
+    '../src/stories/**/*.stories.@(js|jsx|ts|tsx)',
+  ],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
+    '@storybook/addon-docs',
     '@storybook/preset-create-react-app',
   ],
   framework: '@storybook/react',
