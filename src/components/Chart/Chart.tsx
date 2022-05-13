829d53e82c39666d3a55ff1434025b1a66f1847d
oh min seok <oh930428@gmail.com>
2022-05-14 03:12:30 +0900

    [feat] Chart UI 추가 및 스토리 추가

---
+++ src/components/Chart/Chart.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { Header } from '../Header/Header';
+import { Doghnut, ChartDataSetProps } from './Doghnut';
+
+export interface ChartProps {
+  labels: string[];
+  datasets: ChartDataSetProps;
+}
+
+export const Chart = ({ labels, datasets }: ChartProps) => {
+  return (
+    <Container>
+      <div className="header">
+        <Header
+          title="The Most Popular."
+          subTitle="다른 사람들은 취향을 가지고 있을까요?"
+        />
+      </div>
+      <div className="main">
+        <div className="main-section">
+          <Doghnut labels={labels} datasets={datasets} />
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  display: flex;
+  flex-flow: column;
+  margin: 0 auto;
+  max-width: 960px;
+  gap: 40px;
+
+  .main {
+    display: flex;
+    justify-content: space-around;
+    gap: 30px;
+
+    .main-section {
+      padding: 1rem;
+    }
+  }
+`;
